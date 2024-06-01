11400bd71f2474c3e4610a38dc9f9cbaa166892e
IagoGdeMeira <iagomeiraifpr@gmail.com>
2024-06-01 14:03:14 -0300

    novo método de seleção de candidatos

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,10 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
     }
 
     static void analisarCandidato(double salarioPretendido) {
@@ -17,5 +17,30 @@ public class ProcessoSeletivo {
         else
             System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
 
+    }
+
+    static void selecaoCandidatos() {
+        String[] candidatos = {"FELIPE", "MARCIA", "JUNIOR", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+        int candidatosSelecionados = 0;
+        double salarioBase = 2000.0;
+
+        int candidatoAtual = 0;
+        while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.printf("O candidato %s solicitou este valor de salário: R$%.2f\n", candidato, salarioPretendido);
+            
+            if(salarioBase >= salarioPretendido) {
+                System.out.printf("O candidato %s foi selecionado para a vaga\n", candidato);
+                candidatosSelecionados++;
+            }
+            
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 }
